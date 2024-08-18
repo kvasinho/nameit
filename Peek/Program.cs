c028b0318b4813a142d338389a771e19eafcb5f2
kvasinho <kvasinho@googlemail.com>
2024-08-18 21:27:17 +0200

    initial commit

---
+++ Peek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args.Length > 0 ? args[0] : String.Empty;
+            if (String.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("So... Whats you name, you enigmatic guy? ");
+                name = Console.ReadLine() ?? "Mister X";
+            }
+            
+            Console.WriteLine($"Hello {name}");
+           // Console.ReadLine();
+
+        }
+    }
+}
